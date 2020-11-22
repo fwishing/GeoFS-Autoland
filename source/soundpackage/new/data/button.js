2fe711c55aaa46bf1a2123661e96e0e9b93cc729
nicolas377 <68359871+nicolas377@users.noreply.github.com>
2020-11-22 16:28:09 -0500

    update

--- source/soundpackage/new/data/button.js
+++ source/soundpackage/new/data/button.js
@@ -1,35 +1,49 @@
-var automationrunning = 0;
-var soundpackagerunning = 0;
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function dropdown() {
-  document.getElementById("myDropdown").classList.toggle("show");
-};
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      };
-    };
-  };
-};
-function flip(vari) {
-  vari ^= true;
-};
-if (automationrunning==1) {
-    console.log("automation");
-    document.getElementById("automation").classList.add("running");
-} else {
-  document.getElementById("automation").classList.remove("running");
-};
-if (soundpackagerunning==1) {
-    console.log("soundpackage");
-    document.getElementById("soundpackage").classList.add("running");
-} else {
-    document.getElementById("automation").classList.remove("running");
-};
+function dropdown()
+{
+	document.getElementById("myDropdown").classList.toggle("show")
+}
+
+function flip(n)
+{
+	if (n == 0)
+	{
+		n = 1
+	}
+	else
+	{
+		n = 0
+	}
+	console.log(n);
+}
+var automationrunning = 0,
+	soundpackagerunning = 0;
+window.onclick = function (n)
+{
+	if (!n.target.matches(".dropbtn"))
+	{
+		var o, t = document.getElementsByClassName("dropdown-content");
+		for (o = 0; o < t.length; o++)
+		{
+			var e = t[o];
+			e.classList.contains("show") && e.classList.remove("show")
+		}
+	}
+}
+if (automationrunning = 1)
+{
+	console.log("automation");
+	document.getElementById("automation").classList.add("running"))
+}
+else
+{
+	document.getElementById("automation").classList.remove("running"))
+}
+if (soundpackagerunning = 1)
+{
+	console.log("soundpackage");
+	document.getElementById("soundpackage").classList.add("running"))
+}
+else
+{
+	document.getElementById("automation").classList.remove("running")
+}

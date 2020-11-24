481cb5b9bbb09124db76d1ba7022f13853496778
nicolas377 <68359871+nicolas377@users.noreply.github.com>
2020-11-24 08:37:16 -0500

    update

--- source/soundpackage/new/data/update.js
+++ source/soundpackage/new/data/update.js
@@ -1,4 +1,4 @@
-aid=1/*parseInt(geofs.aircraft.instance.id)*/
+aid=1/*geofs.aircraft.instance.id*/
 current_sounds = parsed[aid]
 warning_keys = Object.keys(parsed[aid].warnings)
 gpws_keys = Object.keys(parsed[aid].approach)

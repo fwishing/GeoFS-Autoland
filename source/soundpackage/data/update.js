32e1525c69b4bd42962c459c679ae53efe69a696
nicolas377 <programnicolas@gmail.com>
2020-11-25 09:49:52 -0500

    testing webhook

--- source/soundpackage/data/update.js
+++ source/soundpackage/data/update.js
@@ -1,6 +1,5 @@
 function update() {
   aid=1/*geofs.aircraft.instance.id*/
-  current_sounds = parsed[aid]
   warning_keys = Object.keys(parsed[aid].warnings)
   gpws_keys = Object.keys(parsed[aid].approach)
   base64 = Object.values(parsed[aid].warnings).concat(Object.values(parsed[aid].approach))
